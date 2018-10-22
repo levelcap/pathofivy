c4791c99fd849c8799334cb0711ff8141d35bb79
Dave Cohen <dcohen@zagster.com>
2018-10-22 17:04:44 -0400

    Return nothing on timeout

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -54,7 +54,7 @@ class CharactersController < ApplicationController
       if !@character.last_quest_date.nil?
         minutesSinceLQ = minutesSince(@character.last_quest_date)
         if (minutesSinceLQ < 30)
-          render :nothing => true
+          render body: nil
           return
         end
       end
