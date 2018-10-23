619b883c32a8d0f3350f6c847af0dc9f60e2edd0
Dave Cohen <dcohen@zagster.com>
2018-10-23 11:49:40 -0400

    User model and more options

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -22,19 +22,27 @@ class CharactersController < ApplicationController
     end
 
     @elements = ["Fire", "Wind", "Water", "Earth", "Heart", "Ice", "Void", "Lightning",
-      "Spike", "Life", "Molten", "Storm", "Hug", "Tea", "Stream"]
-    @modifiers = ["shot", "siphon", "wave", "wall", "bounce", "beam", "knife", "sword", "hammer",
-      "strike", "punch", "kick", "spin"]
-    @clazz = ["Tank", "Wizard", "Rogue", "Priest", "Cleric", "Ranger", "Warrior", "Sorceror",
-      "Witch", "Barbarian"]
-    @actions = ["blasted", "fought", "smashed", "smooched", "befriended"]
-    @monsterPart = ["wing", "claw", "fang", "tooth", "eye", "glare",
-      "scale", "talon"]
-    @monsterType = ["imp", "golem", "goblin", "bat", "vampire",
-      "wolf", "spider"]
+      "Spike", "Life", "Molten", "Storm", "Hug", "Tea", "Stream", "Gravel",
+      "Death", "Star", "Sun", "Moon", "Night", "Day"]
 
-    @character = Character.find_by(id: params[:id])
-    @character = Character.find_by(name: params[:id]) if @character.nil?
+    @modifiers = ["shot", "siphon", "wave", "wall", "bounce", "beam", "knife", "sword", "hammer",
+      "strike", "punch", "kick", "spin", "squidge", "drain", "blast", "mace", "spear", "slap"]
+
+    @clazz = ["Tank", "Wizard", "Rogue", "Priest", "Cleric", "Ranger", "Warrior", "Sorceror",
+      "Witch", "Barbarian", "Cultist", "Banker", "Investigator", "NPC", "Warlock"]
+
+    @actions = ["blasted", "fought", "smashed", "smooched", "befriended",
+      "hugged", "obliterated", "did dirty", "offed", "chatted up", "stabbed", "knifed"]
+
+    @monsterPart = ["wing", "claw", "fang", "tooth", "eye", "glare",
+      "scale", "talon", "snarl", "scream", "shriek", "knuckle"]
+
+    @monsterType = ["imp", "golem", "goblin", "bat", "vampire",
+      "wolf", "spider", "kobold", "orc", "elf", "spirit", "demon",
+      "angel", "devil", "dragon", "drake", "gorgon", "slaad", "djinn",
+      "mummy", "ghoul", "peasant", "troll"]
+
+    @character = Character.find_by(id: params[:id]) || Character.find_by(name: params[:id])
 
     if @character.nil?
       randomElement = @elements.sample
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_204714) do
+ActiveRecord::Schema.define(version: 2018_10_23_152421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2018_10_22_204714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "last_quest_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
