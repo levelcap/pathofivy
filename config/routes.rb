05cecdd56b84f9b36db5b219d4c5b376e2df014a
Dave Cohen <dcohen@zagster.com>
2018-12-15 23:19:15 -0500

    Adding the addBoss route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/characters/:channel/:name/sheet', to: 'characters#sheet'
   get '/admin/timeout', to: 'admin#toggleTimeout'
   get '/admin/boss', to: 'admin#bossFight'
+  get '/admin/addboss', to: 'admin#addBoss'
   get '/admin/factions', to: 'admin#factionScore'
   get '/nothing', to: 'characters#nothing'
   root 'welcome#index'
