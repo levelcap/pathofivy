5335cd1e9db6cb1b01581800dd1d00b6622a7c1b
Dave Cohen <dave@bookbub.com>
2019-02-15 17:04:41 -0500

    Adding grab of current viewers from Twitch API

--- app/models/bot_command.rb
+++ app/models/bot_command.rb
@@ -9,27 +9,35 @@ class BotCommand
     @running = false
     @channelName = ENV["CHANNEL_NAME"]
     @timer = Time.now
+    # Hash.new taking a block lets us set default behavior for missing keys
+    # in this case adding them with a chat time and character
     @chatters = Hash.new do |hash, key|
       hash[key] = { :lastChat => Time.now, :character => Questing.new(key, +@channelName).character }
+    end
+
+    # Initialize chatters with current list
+    twitchViewers = fetchTwitchViewers
+    twitchViewers.each do |viewer|
+      @chatters[viewer]
     end
   end
 
   def start
     @running = true
-    Thread.start do
-      talkToMe
-    end
+    # Thread.start do
+    #   talkToMe
+    # end
     Thread.start do
       print "Connected to chat!\n"
       print "#{BOTNAME} Joined ##{CHANNEL}"
       while (@running) do
         ready = IO.select([@socket])
-        # IO.select comes back with an array that has the socket information at index 0 and nothing at all in the rest of the array
-        # I have no idea why, but that is why we do ready[0]
         ready[0].each do |s|
-          line = s.gets
-          unless line.nil?
-            parseLine(line)
+          while line = s.gets
+            unless line.nil?
+              pp line
+              parseLine(line)
+            end
           end
         end
       end
@@ -45,9 +53,17 @@ class BotCommand
   def talkToMe
     while true do
       elapsedSeconds =  Time.now - @timer
-      if (elapsedSeconds > 10)
+      if (elapsedSeconds > 20)
         @timer = Time.now
         if (@running)
+          # remove missing from @chatters to avoid messaging people who have left
+          viewers = fetchTwitchViewers
+          @chatters.keys.each do |key|
+            if (!viewers.include?(key))
+              @chatters.delete(key)
+            end
+          end
+          
           # Get a random person to harass
           sendChannelMessage("#{@chatters.keys.sample} #{@@talkingPoints.sample}")
         end
@@ -59,28 +75,39 @@ class BotCommand
   def parseLine(line)
     # Regex to parse IRC nonsense into something useable
     match = line.match(/^:(.+)!(.+)PRIVMSG ##{@channelName} :(.+)$/)
-    pp match
     original_message = match && match[3]
     if original_message =~ /^/
       original_message.strip!
       message = original_message.downcase
       user = match[1]
-      pp(@chatters[user])
       handleCommands(user, message)
     end
   end
 
   def handleCommands(user, message)
-    if (message.start_with? "!newpath")
-      questing = Questing.new(user, @channelName)
-      adventure = questing.doQuest
-      sendChannelMessage(adventure)
-    elsif (message.start_with? "!otherthing")
-      sendChannelMessage("Hi there this is the other thing")
-    end
+    # if (message.start_with? "!newpath")
+    #   questing = Questing.new(user, @channelName)
+    #   adventure = questing.doQuest
+    #   sendChannelMessage(adventure)
+    # elsif (message.start_with? "!otherthing")
+    #   sendChannelMessage("Hi there this is the other thing")
+    # end
   end
   
   def sendChannelMessage(message)
     @socket.puts("PRIVMSG ##{@channelName} :#{message}")
+  end
+
+  def fetchTwitchViewers
+    begin
+      json = HTTParty.get('http://tmi.twitch.tv/group/user/ivyteapot/chatters')
+      if json
+        response = JSON.parse(json.body)["chatters"]
+        return response["viewers"]
+      end
+    rescue => e
+      pp e.backtrace
+    end
+    return []
   end
 end
