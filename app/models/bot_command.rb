5cfe5e7af276c5b52d2b2e284b6a0dadc1ff268d
Dave Cohen <dave@bookbub.com>
2019-02-15 15:27:30 -0500

    The bot, she talks on her own! MADNESS

--- app/models/bot_command.rb
+++ app/models/bot_command.rb
@@ -1,22 +1,31 @@
 class BotCommand
+  @@talkingPoints = [
+    "My name is IvyTeapot, how do you do?",
+    "Coffee is for the devil.",
+    "Big squidgy internet hugs!"]
+
   def initialize(socket)
     @socket = socket
     @running = false
     @channelName = ENV["CHANNEL_NAME"]
+    @timer = Time.now
   end
 
   def start
     @running = true
     Thread.start do
-      print "Connected to chat!"
+      print "Connected to chat!\n"
       print "#{BOTNAME} Joined ##{CHANNEL}"
+      talkToMe
       while (@running) do
         ready = IO.select([@socket])
         # IO.select comes back with an array that has the socket information at index 0 and nothing at all in the rest of the array
         # I have no idea why, but that is why we do ready[0]
         ready[0].each do |s|
           line = s.gets
-          parseLine(line)
+          unless line.nil?
+            parseLine(line)
+          end
         end
       end
     end
@@ -25,6 +34,20 @@ class BotCommand
   def end
     @socket.puts("PART ##{CHANNEL}\r\n")
     @running = false
+  end
+  
+  private
+  def talkToMe
+    while true do
+      elapsedSeconds =  Time.now - @timer
+      if (elapsedSeconds > 10)
+        @timer = Time.now
+        if (@running)
+          sendChannelMessage(@@talkingPoints.sample)
+        end
+      end
+      sleep 10
+    end
   end
 
   def parseLine(line)
@@ -38,8 +61,7 @@ class BotCommand
       handleCommands(user, message)
     end
   end
-  
-  private
+
   def handleCommands(user, message)
     if (message.start_with? "!newpath")
       questing = Questing.new(user, @channelName)
