cf28c62866bd15bb0df72da9ff2a76deba46e187
Dave Cohen <dave@bookbub.com>
2019-02-15 14:50:00 -0500

    What was I doing? Oh right, a bot

--- app/models/bot_command.rb
+++ app/models/bot_command.rb
@@ -1,32 +1,56 @@
 class BotCommand
-  def parseLine(line, socket)
-    pp CHANNEL
+  def initialize(socket)
+    @socket = socket
+    @running = false
+    @channelName = ENV["CHANNEL_NAME"]
+  end
 
-    match = line.match(/^:(.+)!(.+)PRIVMSG ##{ENV["CHANNEL_NAME"]} :(.+)$/)
+  def start
+    @running = true
+    Thread.start do
+      print "Connected to chat!"
+      print "#{BOTNAME} Joined ##{CHANNEL}"
+      while (@running) do
+        ready = IO.select([@socket])
+        # IO.select comes back with an array that has the socket information at index 0 and nothing at all in the rest of the array
+        # I have no idea why, but that is why we do ready[0]
+        ready[0].each do |s|
+          line = s.gets
+          parseLine(line)
+        end
+      end
+    end
+  end
+
+  def end
+    @socket.puts("PART ##{CHANNEL}\r\n")
+    @running = false
+  end
+
+  def parseLine(line)
+    # Regex to parse IRC nonsense into something useable
+    match = line.match(/^:(.+)!(.+)PRIVMSG ##{@channelName} :(.+)$/)
     original_message = match && match[3]
     if original_message =~ /^/
       original_message.strip!
       message = original_message.downcase
       user = match[1]
-      handleCommands(user, message, socket)
+      handleCommands(user, message)
     end
   end
   
   private
-  def handleCommands(user, message, socket)
+  def handleCommands(user, message)
     if (message.start_with? "!newpath")
-      params = {
-        :id => user,
-        :channel => ENV["CHANNEL_NAME"]
-      }
-      adventure = Questing.allQuestingLogic(params)
-      sendChannelMessage(adventure, socket)
+      questing = Questing.new(user, @channelName)
+      adventure = questing.doQuest
+      sendChannelMessage(adventure)
     elsif (message.start_with? "!otherthing")
-      sendChannelMessage("Hi there this is the other thing", socket)
+      sendChannelMessage("Hi there this is the other thing")
     end
   end
   
-  def sendChannelMessage(message, socket)
-    socket.puts("PRIVMSG ##{ENV["CHANNEL_NAME"]} :#{message}")
+  def sendChannelMessage(message)
+    @socket.puts("PRIVMSG ##{@channelName} :#{message}")
   end
 end
--- config/initializers/twitch.rb
+++ config/initializers/twitch.rb
@@ -3,8 +3,7 @@ Dotenv::Railtie.load
 if defined?(Rails::Server)
   Rails.configuration.after_initialize do
     print("Preparing to connect")
-    botCommand = BotCommand.new
-  
+    
     # Variables
     socket = TCPSocket.new('irc.chat.twitch.tv', 6667)
     running = true
@@ -18,31 +17,9 @@ if defined?(Rails::Server)
     socket.puts("NICK #{BOTNAME}")
     socket.puts("JOIN ##{CHANNEL}")	
     
-    print("Joining #{CHANNEL.capitalize} as #{BOTNAME.capitalize} using OAUTH Token: #{OAUTH[6,OAUTH.length-16]}" + "*"*16)
+    print("Joining #{CHANNEL.capitalize} as #{BOTNAME.capitalize}")
     
     Thread.abort_on_exception = true
-    
-    # Loop (Background Thread) for recieving Twitch chat data
-    Thread.start do
-      print "Connected to chat!"
-      print "#{BOTNAME} Joined ##{CHANNEL}" # Connection Status
-      while (running) do
-        ready = IO.select([socket])
-        ready[0].each do |s|
-          line = s.gets
-          botCommand.parseLine(line, socket)
-        end
-      end
-    end
-    
-    # Loop to keep bot going
-    while (running) do
-        input = gets.chomp
-        if input == "disconnect"
-        socket.puts("PART ##{CHANNEL}\r\n")
-        running = false
-        exit
-      end
-    end
+    botCommand = BotCommand.new(socket).start
   end
 end
